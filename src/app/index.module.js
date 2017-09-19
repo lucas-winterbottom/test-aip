3dad3ba1cdc519f35ae1a8f1972c7c8ea8c1f2d8
Mathew Ly <99143428@student.uts.edu.au>
2017-09-19 12:11:25 +1000

    login/registration forms

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,8 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
+import { AuthController } from './auth/auth.controller';
+import { CompareToDirective } from './directives/compareTo.directive';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 
 
@@ -11,4 +13,6 @@ angular.module('myapp2', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize',
   .config(config)
   .config(routerConfig)
   .controller('MainController', MainController)
+  .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
+  .directive('compareTo', CompareToDirective);
